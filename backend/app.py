f83870336c34f141f46663fb5e0d5a3084eb3745
Shivam Agrawal <shivam@tell.in>
2022-02-19 16:31:35 +0530

    added: api sample routes added

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,15 @@
-from flask import Flask
 
-app = Flask(__name__)
+from flask import Flask, send_from_directory
+from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS #comment this on deployment
+from api.sampleapi import HelloApiHandler
 
-@app.route("/")
-def index():
-    return "Hello World!"
+app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+CORS(app) #comment this on deployment
+api = Api(app)
+
+@app.route("/", defaults={'path':''})
+def serve(path):
+    return send_from_directory(app.static_folder,'index.html')
+
+api.add_resource(HelloApiHandler, '/flask/hello')
