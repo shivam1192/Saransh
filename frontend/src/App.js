9da02630cfff3ceef023c1fdab0f6ca1a329400f
Shivam Agrawal <shivam@tell.in>
2022-02-21 00:34:33 +0530

    added: videos stored in firebase functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {useState} from 'react';
+import storage from './firebase';
+import { getStorage, ref, listAll,getDownloadURL } from "firebase/storage";
 
 function App() {
+const [video , setVideo] = useState('');
+
+const temp = (folder) =>{
+  
+  var listRef =  ref(storage,folder)
+  listAll(listRef)
+  .then((res) => {
+      console.log(res.items[res.items.length - 1]);
+      getDownloadURL(res.items[0]).then((url)=>{
+      console.log(url);
+    });
+  }).catch((error) => {
+    // Uh-oh, an error occurred!
+    console.log(error);
+  }); 
+}
+const upload = ()=>{
+  if(video == null)
+    return;
+  storage.ref(`/videos/${video.name}`).put(video)
+  .on("state_changed" , alert("success") , alert);
+
+  temp("videos/")
+ 
+}
+  
   return (
     <div className="App">
-      Hello world
+      <center>
+      <input type="file" accept='video/*' onChange={(e)=>{setVideo(e.target.files[0])}}/>
+      <button onClick={upload}>Upload</button>
+      </center>
     </div>
   );
 }
-
+  
 export default App;
--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,7 +1,10 @@
-import firebase from 'firebase'
-import 'firebase/storage'
 
-firebase.initializeApp({
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage'; 
+
+const firebaseConfig  = firebase.initializeApp({
   "projectId": "saransh-36252",
   "appId": "1:810027509020:web:c38d4b4ce5ea82a202c6d9",
   "databaseURL": "https://saransh-36252-default-rtdb.firebaseio.com",
@@ -12,4 +15,11 @@ firebase.initializeApp({
   "messagingSenderId": "810027509020",
   "measurementId": "G-B0JCFZ62LK"
 });
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}else {
+  firebase.app(); // if already initialized, use that one
+}
 
+var storage = firebase.storage();
+export default storage;
