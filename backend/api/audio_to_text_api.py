48979a8c2fa929d308857055a6bf8f51b6ac9265
povinsahu1909 <povinsahu@gmail.com>
2022-03-14 02:52:38 +0530

    added helper file

--- backend/api/audio_to_text_api.py
+++ backend/api/audio_to_text_api.py
@@ -1,19 +1,24 @@
 
+from cgitb import strong
 from curses import noecho
 from email import header
 from email.policy import default
-from urllib import request
 from xml.dom.minidom import Document, DocumentFragment
 
 from importlib_metadata import re
+from api.helpers import helper
 from flask_restful import Api, Resource, reqparse
 import requests
+import pyrebase
+from flask import request
 
+
+firebase = pyrebase.initialize_app(helper.config)
+storage = firebase.storage()
 
 class AudioToText(Resource):
   def get(self):
-    request_data = request.get_json()   
-
+    request_data = request.get_json()
     if(request_data['status']==200):
         audioLink = request_data['data-link']
 
@@ -21,7 +26,7 @@ class AudioToText(Resource):
         endpoint = "https://api.assembly.com/v2/transcript"
         audio = {"audioLink" : audioLink}
         headers = {
-            "authorization": key,
+            "authorization": helper.assembly['key'],
             "content-type": "application/json"
         }
         apiResponse = requests.post(endpoint, json = audio, headers = headers)
@@ -36,8 +41,12 @@ class AudioToText(Resource):
         with open(id + '.txt', 'w') as f:
             f.write(textResponse.json()['text'])
       
+        text_link = storage.child("text/" + id).put(id + ".txt")
+        text_link = storage.child("text/" + id).get_url(None)
+        # os.remove(id + ".txt")
         return {
         'resultStatus': 'SUCCESS',
+        'text-link': text_link,
         'message': "audio to text Api Handler"
         }
     else:
--- backend/api/video_to_audio_api.py
+++ backend/api/video_to_audio_api.py
@@ -1,5 +1,4 @@
 import pyrebase
-
 config  = {
   "projectId": "saransh-36252",
   "appId": "1:810027509020:web:c38d4b4ce5ea82a202c6d9",
