45bcd04e91cd1571ce042a9be303b74c00488823
povinsahu1909 <povinsahu@gmail.com>
2022-03-15 00:00:20 +0530

    Fixed api calls and response

--- backend/api/audio_to_text_api.py
+++ backend/api/audio_to_text_api.py
@@ -2,6 +2,7 @@
 from cgitb import strong
 from curses import noecho
 from email import header
+from email.mime import audio
 from email.policy import default
 from xml.dom.minidom import Document, DocumentFragment
 
@@ -11,39 +12,51 @@ from flask_restful import Api, Resource, reqparse
 import requests
 import pyrebase
 from flask import request
-
+import pprint
+from time import sleep
+import os
 
 firebase = pyrebase.initialize_app(helper.config)
 storage = firebase.storage()
 
 class AudioToText(Resource):
   def get(self):
-    request_data = request.get_json()
-    if(request_data['status']==200):
-        audioLink = request_data['data-link']
-
+    audioLink = request.args.get('audiolink', default = None, type = str)
+    token = request.args.get('token', default = None, type = str)
+    
     if audioLink is not None:
-        endpoint = "https://api.assembly.com/v2/transcript"
-        audio = {"audioLink" : audioLink}
+
+        audioParsingIndex = audioLink.find('audio')
+        audioLink = audioLink[0:audioParsingIndex+5] + '%2F' + audioLink[audioParsingIndex+6:] + '&token=' + token
+        endpoint = "https://api.assemblyai.com/v2/transcript"
+        audio = {"audio_url" : audioLink}
         headers = {
             "authorization": helper.assembly['key'],
             "content-type": "application/json"
         }
         apiResponse = requests.post(endpoint, json = audio, headers = headers)
+        print("*"*10 + "\nTranscription request send\n" + "*"*10)
+        pprint.pprint(apiResponse.json())
+
         id = apiResponse.json()['id']
-        textResponse = requests.get(endpoint +"/"+ id, headers)
-        if(textResponse.json()['status'] != "completed"):
-            return {
-                'resultStatus': 'Failed',
-                'status' : 400,
-                'message': "Something went Wrong"
-            }
-        with open(id + '.txt', 'w') as f:
+        apiend = endpoint+"/"+id
+        textResponse = requests.get(apiend, headers = headers)
+        print("*"*10 + "Called for text ouput" + "*"*10)
+        pprint.pprint(textResponse.json())
+
+        filename = textResponse.json()['id'] + '.txt'
+
+        while textResponse.json()['status'] != 'completed':
+            sleep(30)
+            textResponse = requests.get(apiend, headers = headers)
+            print("file is " + textResponse.json()['status'])
+        with open(filename, 'w') as f:
             f.write(textResponse.json()['text'])
       
         text_link = storage.child("text/" + id).put(id + ".txt")
         text_link = storage.child("text/" + id).get_url(None)
-        # os.remove(id + ".txt")
+        os.remove(filename)
+
         return {
         'resultStatus': 'SUCCESS',
         'text-link': text_link,
